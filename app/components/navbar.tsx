51801ffdf6a29fa838c1da0159ec520f45a8c8eb
Laszlo Wagenblast <44098779+LaszloWa@users.noreply.github.com>
2024-10-11 11:14:25 +0200

    updated styling to keep fixed position and remove elements from flow

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,16 +3,15 @@
 import React, { useState } from "react";
 import styles from "./navbar.module.css";
 
-const Navbar = () => {
-	const [isOpen, setIsOpen] = useState(false);
+const Navbar: React.FC = () => {
+	const [isOpen, setIsOpen] = useState<boolean>(false);
 
-	// Toggle the menu open/close state
 	const toggleMenu = () => {
 		setIsOpen(!isOpen);
 	};
 
 	return (
-		<nav className={styles.navbar}>
+		<>
 			<div className={styles.hamburger} onClick={toggleMenu}>
 				<div
 					className={`${styles.line} ${isOpen ? styles.line1Open : ""}`}
@@ -25,27 +24,29 @@ const Navbar = () => {
 				></div>
 			</div>
 
-			<ul className={`${styles.menu} ${isOpen ? styles.menuOpen : ""}`}>
-				<li>
-					<a href="/">Home</a>
-				</li>
-				<li>
-					<a href="/ablauf">Ablauf</a>
-				</li>
-				<li>
-					<a href="/rsvp">RSVP</a>
-				</li>
-				<li>
-					<a href="/location">Location</a>
-				</li>
-				<li>
-					<a href="/uebernachtung">Übernachtung</a>
-				</li>
-				<li>
-					<a href="/faq">Fragen & Antworten</a>
-				</li>
-			</ul>
-		</nav>
+			<div className={`${styles.menu} ${isOpen ? styles.menuOpen : ""}`}>
+				<ul>
+					<li>
+						<a href="/">Home</a>
+					</li>
+					<li>
+						<a href="/ablauf">Ablauf</a>
+					</li>
+					<li>
+						<a href="/rsvp">RSVP</a>
+					</li>
+					<li>
+						<a href="/location">Location</a>
+					</li>
+					<li>
+						<a href="/uebernachtung">Übernachtung</a>
+					</li>
+					<li>
+						<a href="/faq">Fragen & Antworten</a>
+					</li>
+				</ul>
+			</div>
+		</>
 	);
 };
 
