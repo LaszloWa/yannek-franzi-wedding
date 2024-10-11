65c74518fa98d9d9efbef875b513ea37b4f4da63
Laszlo Wagenblast <44098779+LaszloWa@users.noreply.github.com>
2024-10-11 11:13:46 +0200

    Removes unused stylesheet

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import styles from "./layout.module.css";
 import Navbar from "./components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
