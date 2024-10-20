3383f3975bc9fb98d7021a13bff98285a17c0785
Laszlo Wagenblast <44098779+LaszloWa@users.noreply.github.com>
2024-10-20 17:12:54 +0200

    styles rsvp form

--- app/rsvp/page.tsx
+++ app/rsvp/page.tsx
@@ -4,49 +4,70 @@ export default function Rsvp() {
 	return (
 		<main className={styles.main}>
 			<h1>Rückmeldung</h1>
-			<div>
-				Bitte gebt uns eure Rückmeldung über das untenstehende Formular bis zum
-				30.06.2025! Alternativ dürft ihr uns auch gerne telefonisch oder per
-				E-Mail kontaktieren. Es genügt, wenn ihr das Formular pro Einladung
-				ausfüllt.
-			</div>
-			<form className={styles.form} action="">
-				<div>
-					<label htmlFor="name">Name</label>
+			<form className={styles.form} action="/api/send-email" method="POST">
+				<div className={styles.description}>
+					Bitte gebt uns eure Rückmeldung über das untenstehende Formular bis
+					zum 30.06.2025! Alternativ dürft ihr uns auch gerne telefonisch oder
+					per E-Mail kontaktieren. Es genügt, wenn ihr das Formular pro
+					Einladung ausfüllt.
+				</div>
+				<div className={styles.formSection}>
+					<label htmlFor="name">
+						<b>Name</b>
+					</label>
+					<br />
 					<input type="text" id="name" name="name" />
 				</div>
-				<div>
-					<label htmlFor="anreiseTag">Anreisetag</label>
-					<select name="anreiseTag" id="anreiseTag">
+				<div className={styles.formSection}>
+					<label htmlFor="anreiseTag">
+						<b>Anreisetag</b>
+					</label>
+					<br />
+					<select name="anreiseTag" id="anreiseTag" className={styles.button}>
 						<option value="">Wãhle deinen Anreisetag</option>
 						<option value="Freitag">Freitag</option>
 						<option value="Samstag">Samstag</option>
 					</select>
 				</div>
-				<div>
-					<span>Übernachtung</span>
-					<div>
+				<div className={styles.radio}>
+					<span>
+						<b>Übernachtung</b>
+					</span>
+
+					<div className={styles.radioInput}>
+						<input
+							type="radio"
+							id="zweiNaechte"
+							name="hotel"
+							value="zweiNaechte"
+						/>
 						<label htmlFor="zweiNaechte">
 							Ja, ich/wir übernachten von Freitag bis Sonntag.
 						</label>
-						<input type="radio" id="zweiNaechte" name="hotel" />
 					</div>
-					<div>
+
+					<div className={styles.radioInput}>
+						<input type="radio" id="eineNacht" name="hotel" value="eineNacht" />
 						<label htmlFor="eineNacht">
 							Ja, ich/wir übernachten von Samstag bis Sonntag.
 						</label>
-						<input type="radio" id="eineNacht" name="hotel" />
 					</div>
-					<div>
-						<label htmlFor="Nein">Nein, ich/wir übernachten nicht,</label>
-						<input type="radio" id="Nein" name="hotel" />
+
+					<div className={styles.radioInput}>
+						<input type="radio" id="Nein" name="hotel" value="Nein" />
+						<label htmlFor="Nein">Nein, ich/wir übernachten nicht.</label>
 					</div>
 				</div>
-				<div>
-					<label htmlFor="kommentar">Sonstiges was wir wissen sollten?</label>
+				<div className={styles.formSection}>
+					<label htmlFor="kommentar">
+						<b>Sonstiges was wir wissen sollten?</b>
+					</label>
+					<br />
 					<textarea id="kommentar" name="kommentar"></textarea>
 				</div>
-				<button type="submit">Und Abfahrt!</button>
+				<button type="submit" className={styles.button}>
+					Und Abfahrt!
+				</button>
 			</form>
 		</main>
 	);
