c36ea9817b9e03af90a4003e11517aa7df955f92
Laszlo Wagenblast <44098779+LaszloWa@users.noreply.github.com>
2024-10-21 22:16:21 +0200

    Text styling main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import styles from "./page.module.css";
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<div>
-				Willkommen auf unserer Hochzeitshomepage. Hier findet ihr alle wichtigen
-				Infos. Wir freuen uns sehr, unser Hochzeitswochenende mit euch zu
-				feiern!
+			<div className={styles.description}>
+				Willkommen auf unserer Hochzeitshomepage.
+				<br />
+				<br />
+				Hier findet ihr alle wichtigen Infos. Wir freuen uns sehr, unser
+				Hochzeitswochenende mit euch zu feiern!
 			</div>
 			<div className={styles.image}></div>
 		</main>
